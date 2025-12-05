e2c26a2bf69df2e7aec43a6a42eacfdef9278383
zadriskill@gmail.com <zachdrisk@Zachs-MacBook-Air-144.local>
2025-12-05 16:08:25 -0700

    start working on timing code

---
+++ mcu_fpga/hw/send_guess.sv
@@ -0,0 +1,164 @@
+
+module send_guess #(
+    parameter int CODE_LEN = 4
+) (
+    input  wire logic CLK_50,
+    input  wire logic CLK_inter,   // MCU-driven interconnect clock
+    input  wire logic [0:0]  SW,          // active-high reset
+    inout  logic [7:0]  CM,          // bidirectional bus
+    input  logic [31:0] guess,
+    input  logic begin_transaction,
+    output logic waiting_for_reply,
+    output logic [7:0] data_from_mcu
+);
+
+    // Protocol bytes
+    localparam [7:0] START_BYTE         = 8'h01;
+    localparam [7:0] BEGIN_GUESSING     = 8'h02;
+    localparam [7:0] YES                = 8'h03;
+    localparam [7:0] NO                 = 8'h04;
+    localparam [7:0] END_BYTE           = 8'h05;
+    localparam [7:0] START_GUESS_RANGE  = 8'h06;
+
+    // FSM states
+    typedef enum logic [2:0] {
+        IDLE,
+        WAIT_BEFORE_START,
+        SEND_START,
+        SEND_DATA,
+        SEND_END,
+        SHORT_DELAY,
+        WAIT_REPLY,
+    } state_t;
+
+    state_t current_state, next_state;
+
+    // CM data drive signals
+    logic  [7:0] data_out;
+    logic        drive_en;
+    logic [7:0] data_in;
+
+    // CM bus output
+    assign CM = drive_en ? data_out : 8'bZ;
+
+    // CM bus input Sychronizer
+    reg [7:0] data_in_sync0, data_in_sync1;
+    assign data_in = CM;
+
+    always_ff @(posedge CLK_50) begin
+        data_in_sync0 <= data_in;
+        data_in_sync1 <= data_in_sync0;
+    end
+
+    assign data_from_mcu = data_in_sync1;
+
+    // detect edges of clk_inter (synchronized into CLK_50 domain)
+    logic clk_inter_sync0, clk_inter_sync1;
+    always_ff @(posedge CLK_50) begin
+        clk_inter_sync0 <= CLK_inter;
+        clk_inter_sync1 <= clk_inter_sync0;
+    end
+    logic clk_inter_rise =  clk_inter_sync0 & ~clk_inter_sync1;
+    logic clk_inter_fall = ~clk_inter_sync0 &  clk_inter_sync1;
+
+
+    // byte counter for sending CODE_LEN bytes
+    localparam int BYTE_COUNT_WIDTH = (CODE_LEN > 1) ? $clog2(CODE_LEN) : 1;
+    logic [BYTE_COUNT_WIDTH-1:0] byte_count;
+    always_ff @(posedge CLK_50) begin
+        if (SW[0])
+            byte_count <= '0;
+        else begin
+            if (current_state != SEND_DATA)
+                byte_count <= '0;
+            else if (current_state == SEND_DATA && clk_inter_fall)
+                byte_count <= byte_count + 1'b1;
+        end
+    end
+
+
+    // Next-state logic
+    always_comb begin
+        next_state = current_state; // default to hold
+        case (current_state)
+            IDLE: begin
+                if (begin_transaction)
+                    next_state = WAIT_BEFORE_START;
+            end
+
+            WAIT_BEFORE_START: begin
+                if (clk_inter_fall) 
+                    next_state = SEND_START;
+            end
+
+            SEND_START: begin
+                // hold START for one pulse then move to SEND_DATA
+                if (clk_inter_fall)
+                    next_state = SEND_DATA;
+            end
+
+            SEND_DATA: begin
+                if (clk_inter_fall) begin
+                    if (byte_count == CODE_LEN-1)
+                        next_state = SEND_END;
+                    else
+                        next_state = SEND_DATA;
+                end
+            end
+           
+            SEND_END: begin
+                if (clk_inter_fall)
+                    next_state = SHORT_DELAY;
+            end
+
+            SHORT_DELAY: begin
+                    next_state = WAIT_REPLY;
+            end
+
+            WAIT_REPLY: begin
+                if (data_in_sync1 == YES || data_in_sync1 == NO)
+                    next_state = IDLE;
+            end
+
+            default: next_state = IDLE;
+        endcase
+    end
+
+    // Moore outputs
+    always @(*) begin
+        drive_en = 1'b0;
+        data_out = 8'h00;
+        waiting_for_reply = 1'b0;
+
+        case (current_state)
+            SEND_START: begin
+                drive_en = 1'b1;
+                data_out = START_BYTE;
+            end
+            SEND_DATA: begin
+                drive_en = 1;
+                data_out = guess[((byte_count+1)*8)-1:byte_count*8];
+            end
+            SEND_END: begin
+                drive_en = 1;
+                data_out = END_BYTE;
+            end
+            WAIT_REPLY: begin
+                waiting_for_reply = 1'b1;
+            end
+        endcase
+    end
+
+
+    // State register
+    always @(posedge CLK_50) begin
+        if (SW[0])
+            current_state <= IDLE;
+        else
+            current_state <= next_state;
+    end
+
+   
+
+endmodule
+
---
+++ mcu_fpga/hw/top.sv
@@ -0,0 +1,239 @@
+
+module top  #(
+    parameter int CODE_LEN = 4
+) (
+    input  wire logic CLK_50,
+    input  wire logic CLK_inter,   // MCU-driven interconnect clock
+    input  wire logic [0:0]  SW,          // active-high reset
+    inout  logic [7:0]  CM,          // bidirectional bus
+    output logic [7:0] LED;
+);
+    // Protocol bytes
+    localparam [7:0] START_BYTE         = 8'h01;
+    localparam [7:0] BEGIN_GUESSING     = 8'h02;
+    localparam [7:0] YES                = 8'h03;
+    localparam [7:0] NO                 = 8'h04;
+    localparam [7:0] END_BYTE           = 8'h05;
+    localparam [7:0] START_GUESS_RANGE  = 8'h06;
+
+    // State machine states
+    typedef enum logic [2:0] {
+        IDLE,
+        SEND_GUESS,
+        COUNT_DELAY,
+        STORE_DELAY,
+        SET_CORREECT_GUESS,
+        RESET_FOR_NEXT_BYTE
+    } state_t;
+
+    state_t current_state, next_state;
+
+    // Instantiate send_guess module
+    logic [(CODE_LEN*8)-1:0] guess_word;
+    logic waiting_for_reply;
+    logic begin_transaction;
+    logic [7:0] data_from_mcu;
+
+    send_guess send_guess_inst #(
+        .CODE_LEN(CODE_LEN)
+    ) (
+        .CLK_50(CLK_50),
+        .CLK_inter(CLK_inter),
+        .SW(SW),
+        .CM(CM),
+        .guess(guess_word),
+        .begin_transaction(begin_transaction),
+        .waiting_for_reply(waiting_for_reply),
+        .data_from_mcu(data_from_mcu)
+    );
+
+    // Guess byte increment logic
+    logic [7:0] guess_byte;
+    logic inc_guess_byte;
+    logic inc_guess_byte_reg;
+    logic reset_guess_byte;
+    
+    always_ff @(posedge CLK_50) begin
+        if (SW[0]) begin
+            guess_byte <= START_GUESS_RANGE;
+        end else begin
+            if (reset_guess_byte) begin
+                guess_byte <= START_GUESS_RANGE;
+            end else if (inc_guess_byte_reg) begin
+                if (guess_byte == 8'hFF)
+                    guess_byte <= START_GUESS_RANGE;
+                else
+                    guess_byte <= guess_byte + 1'b1;
+            end
+        end
+    end
+
+    // Inc register for Safety
+    always_ff @(posedge CLK_50) begin
+        inc_guess_byte_reg <= inc_guess_byte;
+    end
+
+    // Byte counter
+    localparam int BYTE_COUNT_WIDTH = (CODE_LEN > 1) ? $clog2(CODE_LEN) : 1;
+    logic [BYTE_COUNT_WIDTH-1:0] byte_counter;
+    logic inc_byte_counter;
+
+    always_ff @(posedge CLK_50) begin
+        if (SW[0]) begin
+            byte_counter <= 2'd0;
+        end else if (inc_byte_counter) begin
+            byte_counter <= byte_counter + 1'b1;
+        end
+    end
+
+    // Delay Counter
+    logic [31:0] delay_counter;
+    logic reset_delay_counter;
+    logic inc_delay_counter;
+
+    always_ff @(posedge CLK_50) begin
+        if (SW[0])
+            delay_counter <= 32'd0;
+        else begin
+            if (reset_delay_counter)
+                delay_counter <= 32'd0;
+            else if (inc_delay_counter)
+                delay_counter <= delay_counter + 1'b1;
+        end
+    end
+
+    // Memory to store delays
+    logic [31:0] delay_memory [0:255];
+    logic store_byte;
+
+    always_ff @(posedge CLK_50) begin
+        if (store_byte) begin
+            delay_memory[guess_byte] <= delay_counter;
+        end
+    end
+
+    // Correct Bytes memory
+    logic [7:0] correct_bytes [CODE_LEN-1:0];
+
+    always_comb begin
+        for (int i = 0; i < CODE_LEN; i++) begin
+            guess_word[((i+1)*8)-1:i*8] = correct_bytes[i];
+        end
+
+        guess_word[((byte_counter+1)*8)-1:byte_counter*8] = guess_byte;
+    end
+
+
+    // Process to search for the byte that had the longest delay time
+    always_ff @(posedge CLK_50) begin
+        if (SW[0]) begin
+            for (int i = 0; i < CODE_LEN; i++) begin
+                correct_bytes[i] <= 8'h00;
+            end
+        end
+        else begin
+            if (current_state == SET_CORRECT_GUESS) begin
+                // find max
+                integer i;
+                logic [31:0] max_delay;
+                logic [7:0]  max_byte;
+                max_delay = 32'd0;
+                max_byte  = 8'd0;
+                for (i = 0; i < 256; i = i + 1) begin
+                    if (delay_memory[i] > max_delay) begin
+                        max_delay = delay_memory[i];
+                        max_byte  = i[7:0];
+                    end
+                end
+                correct_bytes[byte_counter] <= max_byte;
+            end
+        end
+    end
+
+    // State Machine transition logic and outputs
+    always_comb begin
+        // Default assignments
+        begin_transaction = 1'b0;
+        reset_delay_counter = 1'b0;
+        inc_delay_counter = 1'b0;
+        store_byte = 1'b0;
+        inc_guess_byte      = 1'b0;
+        reset_guess_byte    = 1'b0;
+        inc_byte_counter    = 1'b0;
+        next_state = current_state;
+
+        case (current_state)
+            IDLE: begin
+                if (data_from_mcu == BEGIN_GUESSING) begin
+                    next_state = SEND_GUESS;
+                end else begin
+                    next_state = IDLE;
+                end
+            end
+
+            SEND_GUESS: begin
+                // outputs
+                begin_transaction = 1'b1;
+                reset_delay_counter = 1'b1;
+
+                // transition logic
+                if (waiting_for_reply)
+                    next_state = COUNT_DELAY;
+            end
+
+            COUNT_DELAY: begin
+                // outputs
+                inc_delay_counter = 1'b1;
+
+                // transition logic
+                if (!waiting_for_reply)
+                    next_state = STORE_DELAY;
+            end
+
+            STORE_DELAY: begin
+                // outputs
+                store_byte = 1'b1;
+
+                // transition logic
+                if (guess_byte == 8'hFF) begin
+                    next_state = SET_CORREECT_GUESS;
+                end else begin
+                    inc_guess_byte = 1'b1;
+                    next_state = SEND_GUESS;
+                end
+            end
+
+            SET_CORREECT_GUESS: begin
+
+                // transition logic
+                next_state = RESET_FOR_NEXT_BYTE;
+            end
+
+            RESET_FOR_NEXT_BYTE: begin
+                // outputs
+                reset_guess_byte = 1'b1;
+                inc_byte_counter = 1'b1;
+
+                // transition logic
+                if (byte_counter == CODE_LEN-1)
+                    next_state = IDLE;
+                else
+                    next_state = SEND_GUESS;
+            end
+
+            default: next_state = IDLE;
+        endcase
+    end
+
+    // State register
+    always_ff @(posedge CLK_50) begin
+        if (SW[0])
+            current_state <= IDLE;
+        else
+            current_state <= next_state;
+    end
+    
+    // LED output for debugging
+    assign LED = guess_byte;
+
+endmodule
